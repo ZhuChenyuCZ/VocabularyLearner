62edbc22eae1a1262f5ce044e06840657f7027c0
ZhuChenyuCZ <1125664127@qq.com>
2020-07-20 18:26:16 +0800

    无尽模式

--- v0.0.0/SQL.py
+++ v0.0.0/SQL.py
@@ -3,3 +3,6 @@ Words = []
 Type = []
 # 0 表示单词 1表示账号 2 表示其他
 Meaning = []
+WrongTime = []
+Weight = []
+Hash = []
--- v0.0.0/VocabularyLearner.py
+++ v0.0.0/VocabularyLearner.py
@@ -21,17 +21,22 @@ def SQLRead():
         TempSaveArray.append(line)
         TempLen+=1
 
-    while SQL.TotalLen*3<TempLen:
+    while SQL.TotalLen*5<TempLen:
         SQL.TotalLen+=1
         SQL.Words.append('save')
         SQL.Type.append(0)
         SQL.Meaning.append('占位')
+        SQL.WrongTime.append(0)
+        SQL.Weight.append(1)
+        SQL.Hash.append(0)
     
     i=0
-    while 3*i<TempLen:
-        SQL.Words[i]=TempSaveArray[3*i]
-        SQL.Type[i]=TempSaveArray[3*i+1]
-        SQL.Meaning[i]=TempSaveArray[3*i+2]
+    while 5*i<TempLen:
+        SQL.Words[i]=TempSaveArray[5*i]
+        SQL.Type[i]=int(TempSaveArray[5*i+1])
+        SQL.Meaning[i]=TempSaveArray[5*i+2]
+        SQL.WrongTime[i]=int(TempSaveArray[5*i+3])
+        SQL.Weight[i]=int(TempSaveArray[5*i+4])
         i+=1
 
     textfile.close()
@@ -42,8 +47,10 @@ def SQLWrite():
     textfile=open("SaveFile.txt","w")
     for i in range(0,SQL.TotalLen):
         textfile.write(SQL.Words[i])
-        textfile.write(SQL.Type[i])
+        textfile.write('%s\n' %str(SQL.Type[i]))
         textfile.write(SQL.Meaning[i])
+        textfile.write('%s\n' % str(SQL.WrongTime[i]))
+        textfile.write('%s\n' % str(SQL.Weight[i]))
     textfile.close()
 
 def AddWords():
@@ -56,14 +63,40 @@ def AddWords():
     NewWordsMeaning += '\n'
     SQL.TotalLen+=1
     SQL.Words.append(NewWords)
-    SQL.Type.append('0\n')
+    SQL.Type.append(0)
     SQL.Meaning.append(NewWordsMeaning)
+    SQL.WrongTime.append(0)
+    SQL.Weight.append(1)
+    SQL.Hash.append(0)
 
 def ChooseMode():
-    print('添加新单词输入1 ， 测试以往单词输入2 ,退出请输入 0')
+    print('添加新单词输入1 ， 测试以往单词输入2 ,测试单词无限模式输入3，退出请输入 0')
     base.ModeType = int(input('Mode:'))
-    if base.ModeType!=1 and base.ModeType!=2:
+    if base.ModeType<1 or base.ModeType>3:
         base.ModeType = -1
+
+def InfiniteTestMode():
+    print('进入无尽模式')
+    i=0
+    while i<SQL.TotalLen:
+        NowID = math.floor(random.random()*SQL.TotalLen-0.0001)
+        """while SQL.Hash[NowID] == 1:
+            NowID = math.floor(random.random()*SQL.TotalLen-0.001)"""
+        print('NowID:%d' % NowID)
+        print('请问这个单词是什么意思: %s' % SQL.Words[NowID] , end = '')
+        TempStr = str(input('输入任何字符查看解释'))
+        print(SQL.Meaning[NowID])
+        TempStr = str(input('下一个单词输入回车，保留这个单词输入r，退出输入e:'))
+        if TempStr == 'e':
+            break
+        elif TempStr == 'r':
+            SQL.Hash[NowID] = 0
+        else:
+            SQL.Hash[NowID] = 1
+            i += 1
+        print('next')
+    print('退出无尽模式')
+    base.ModeType = 0
 
 def main():
     ShowTheWelcomePage()
@@ -100,6 +133,9 @@ def main():
                 print('       %s' % SQL.Meaning[NowWords])
                 TempStr = str(input('输入任何字符测试下一个'))
             base.ModeType = 0
+        elif base.ModeType == 3:
+            InfiniteTestMode()
+                
 
 if __name__ == '__main__':
     main()
